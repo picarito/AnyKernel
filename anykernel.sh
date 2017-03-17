b16da76bc7805774cf5b178a8179ea9942a3f85e
Akuma70 <mohammad.parsa1370@gmail.com>
2017-03-17 12:33:44 +0330

    anykernel: add Guyver initialization script

--- anykernel.sh
+++ anykernel.sh
@@ -3,19 +3,19 @@
 
 ## AnyKernel setup
 # EDIFY properties
-kernel.string=DirtyV by bsmitty83 @ xda-developers
+kernel.string=Guyver by MEGAX91 @ xda-developers
 do.devicecheck=1
+do.initd=1
 do.modules=0
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
+device.name1=kate
+device.name2=kenzo
+device.name3=Redmi Note 3
 device.name4=
 device.name5=
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 
 
@@ -27,7 +27,6 @@ is_slot_device=0;
 ## AnyKernel permissions
 # set permissions for included ramdisk files
 chmod -R 755 $ramdisk
-chmod 644 $ramdisk/sbin/media_profiles.xml
 
 
 ## AnyKernel install
@@ -35,32 +34,22 @@ dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-append_file init.rc "run-parts" init;
+# add guyver initialization script
+insert_line init.rc "import /init.guyver.rc" after "import /init.cm.rc" "import /init.guyver.rc";
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "dvbootscript" init.tuna;
+# fstab default to noatime
+patch_fstab fstab.qcom /data f2fs options "nosuid,nodev,noatime,nodiratime,inline_xattr,data_flush                        wait,check,encryptable=footer,formattable,length=-16384" "nosuid,nodev,noatime,inline_xattr,data_flush                        wait,check,encryptable=footer,formattable,length=-16384"
+patch_fstab fstab.qcom /data ext4 options "nosuid,nodev,noatime,nodiratime,barrier=1,noauto_da_alloc                      wait,check,encryptable=footer,formattable,length=-16384" "nosuid,nodev,noatime,barrier=1,noauto_da_alloc                      wait,check,encryptable=footer,formattable,length=-16384"
+patch_fstab fstab.qcom /cache f2fs options "nosuid,nodev,noatime,nodiratime,inline_xattr,flush_merge,data_flush            wait,check,formattable" "nosuid,nodev,noatime,inline_xattr,flush_merge,data_flush            wait,check,formattable"
+patch_fstab fstab.qcom /cache ext4 options "nosuid,nodev,noatime,nodiratime,barrier=1                                      wait,check,formattable" "nosuid,nodev,noatime,barrier=1                                      wait,check,formattable"
 
-# init.superuser.rc
-if [ -f init.superuser.rc ]; then
-  backup_file init.superuser.rc;
-  replace_string init.superuser.rc "Superuser su_daemon" "# su daemon" "\n# Superuser su_daemon";
-  prepend_file init.superuser.rc "SuperSU daemonsu" init.superuser;
-else
-  replace_file init.superuser.rc 750 init.superuser.rc;
-  insert_line init.rc "init.superuser.rc" after "on post-fs-data" "    import /init.superuser.rc";
-fi;
+mount -o rw,remount -t auto /system;
+rm -f /system/lib/modules/wlan.ko;
+cp -f /tmp/anykernel/wlan.ko /system/lib/modules/wlan.ko;
+cp -f /tmp/anykernel/wlan.ko /system/lib/modules/pronto/pronto_wlan.ko;
+chmod 0644 /system/lib/modules/wlan.ko;
 
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "nodiratime,barrier=0" "nodev,noatime,nodiratime,barrier=0,data=writeback,noauto_da_alloc,discard";
-patch_fstab fstab.tuna /cache ext4 options "barrier=0,nomblk_io_submit" "nosuid,nodev,noatime,nodiratime,errors=panic,barrier=0,nomblk_io_submit,data=writeback,noauto_da_alloc";
-patch_fstab fstab.tuna /data ext4 options "nomblk_io_submit,data=writeback" "nosuid,nodev,noatime,errors=panic,nomblk_io_submit,data=writeback,noauto_da_alloc";
-append_file fstab.tuna "usbdisk" fstab;
+mount -o ro,remount -t auto /system;
 
 # end ramdisk changes
 
